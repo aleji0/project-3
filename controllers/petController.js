837296b501b6cab68bba7b11dd3ab84b835b0278
aleji0 <alex.springer@gmail.com>
2022-11-27 12:47:48 -0700

    clarfiying pseudo-code

--- controllers/petController.js
+++ controllers/petController.js
@@ -11,7 +11,7 @@ const petController = {
                 res.status(400).json(err);            
             });
     },
-    //get pet by ID
+    //get pet by ID if that's even a thing we need to do... if not, it can be a template/placeholder
     getPetById({ params }, res) {
         Pets.findone({ _id: params.petId })
             .then(petData => res.json(petData))
