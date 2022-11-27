f372d79591c2ced73bb4fbecf193c4dc1577a552
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-27 16:59:34 -0700

    front end, back end cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
-<<<<<<< HEAD
 import Login from './components/Login';
-
-function App() {
-  return (
-    <div>
-      <Login />
-=======
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -18,23 +12,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 06e0e281dcccea1f7189ff309f88f32cf125261a
-    </div>
+      <Login />
     </ApolloProvider>
   );
 }
