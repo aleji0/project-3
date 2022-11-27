53ce3067b7cde70905ac1c52b2f96f25f8c95086
aleji0 <alex.springer@gmail.com>
2022-11-27 12:24:29 -0700

    continuing on controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,16 @@
 const {Users} = require('../models');
 
 const userController = {
-    Users.create({body}, res) {
+    
+    createUser({body}, res) {
+        Users.create(body)
         .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // get all users
+    getUsers(req, res) {
+        Users.find({})
+
     }
-
-
 }
