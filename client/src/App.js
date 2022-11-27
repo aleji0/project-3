c21f0c6c87ca1ba2a1fc80f0b21cc3fc8a59ab6a
Ace <106693166+rickyavila29@users.noreply.github.com>
2022-11-27 08:28:34 -0700

    type defs and resolvers

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +27,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ApolloProvider>
   );
 }
 
