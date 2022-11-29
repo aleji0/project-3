b60034a5629fa2740a164d65627fa0d322db847d
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-28 19:33:26 -0700

    created form for input of pet's name

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,16 +12,6 @@ export const ADD_PROFILE = gql`
   }
 `;
 
-export const ADD_SKILL = gql`
-  mutation addSkill($profileId: ID!, $skill: String!) {
-    addSkill(profileId: $profileId, skill: $skill) {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -31,16 +21,6 @@ export const LOGIN_USER = gql`
         firstName
         lastName
       }
-    }
-  }
-`;
-
-export const REMOVE_SKILL = gql`
-  mutation removeSkill($skill: String!) {
-    removeSkill(skill: $skill) {
-      _id
-      name
-      skills
     }
   }
 `;
