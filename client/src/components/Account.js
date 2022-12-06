0d6e53ed8a78bbf8d87044f50501f9d009a78c7f
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-05 17:56:06 -0700

    added card to account.js

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -178,41 +178,18 @@ const Account = () => {
             </Button>
           </Box>
         </Box>
-        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          <Box sx={{ my: 3, mx: 2 }}>
-            <Grid container alignItems="center">
-              <Grid item xs>
-                <Typography gutterBottom variant="h4" component="div">
-                  Toothbrush
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography gutterBottom variant="h6" component="div">
-                  $4.50
-                </Typography>
-              </Grid>
-            </Grid>
-            <Typography color="text.secondary" variant="body2">
-              Pinstriped cornflower blue cotton blouse takes you on a walk to the park or
-              just down the hall.
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              Healthy Pets
             </Typography>
-          </Box>
-          <Divider variant="middle" />
-          <Box sx={{ m: 2 }}>
-            <Typography gutterBottom variant="body1">
-              Select type
-            </Typography>
-            <Stack direction="row" spacing={1}>
-              <Chip label="Extra Soft" />
-              <Chip color="primary" label="Soft" />
-              <Chip label="Medium" />
-              <Chip label="Hard" />
-            </Stack>
-          </Box>
-          <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
-            <Button>Add to cart</Button>
-          </Box>
-        </Box>
+
+            <Typography variant="body2"></Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       </Container>
     </ThemeProvider>
   );
