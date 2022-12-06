b9e61d94d025c0545f3e4305927cc377b1a17b22
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-05 20:30:10 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Getting started
+
 - Pull current repo
 - create .env with proper values from .env.example file in ./server (values in Slack)
 - npm i from root level
@@ -7,8 +8,8 @@
 - Open graphql and login
 - Copy token to environment variable
 
-
 # project-3
+
 - reset auth in server (see diff)
 - add utils back in
 - create .env [npm github](https://github.com/motdotla/dotenv#readme)
@@ -19,11 +20,11 @@
 - Authentication in the [Apollo Sandbox](https://www.apollographql.com/docs/graphos/explorer/connecting-authenticating/)
 
 ### Resources
+
 - Synching up with a [remote branch](https://www.ocpsoft.org/tutorials/git/reset-and-sync-local-respository-with-remote-branch/)
 
 <p>&nbsp;</p>
 <p>&nbsp;</p>
-
 
 -<img src="https://www.preventivevet.com/hs-fs/hubfs/Pv-homepage-dogCat.jpg?width=1200&height=400&name=Pv-homepage-dogCat.jpg">
 
@@ -48,9 +49,9 @@
 
 # User Story
 
-    - AS A
-    - I WANT
-    - SO THAT I
+    - AS A pet owner
+    - I WANT an app that I can enter my pets vital info
+    - SO THAT I can track and monitor my pets health
 
 <p>&nbsp;</p>
 
@@ -84,9 +85,19 @@
 
 # Description
 
+<h2>
+When you first pull up Healthy Pets, you will see a login screen.  Enter your email, password and hit submit.
+<img src="https://user-images.githubusercontent.com/107449948/205804046-e86b1a0b-ab5f-4a29-9cb6-fed4be723400.png">
+<p>&nbsp;</p>
+
+You will be taken to your pet info screen. Enter your pets info by answering the questions bellow.
+<img src="https://user-images.githubusercontent.com/107449948/205804049-be5bf4d9-55ac-4384-9c0d-6724346a16c6.png">
+
 <p>&nbsp;</p>
 
 # Technology
+
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![NPM](https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white) ![MUI](https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white) ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white) ![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB) ![Insomnia](https://img.shields.io/badge/Insomnia-black?style=for-the-badge&logo=insomnia&logoColor=5849BE) ![Heroku](https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white)
 
 <p>&nbsp;</p>
 
@@ -95,6 +106,10 @@
 <p>&nbsp;</p>
 
 # Credits
+
+| Sammy Pennell                                                                                                                        | Shannon Holland                                                                                                                     | Alex Springer                                                                                                                       | Ricardo Avila                                                                                                                             |
+| ------------------------------------------------------------------------------------------------------------------------------------ | ----------------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------------------------------------------------- |
+| [<img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white">](https://github.com/SammyDP) | [<img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white">](https://github.com/sm0526) | [<img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white">](https://github.com/aleji0) | [<img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white">](https://github.com/rickyavila29) |
 
 <p>&nbsp;</p>
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,31 +1,36 @@
-import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import { AiFillLock } from 'react-icons/ai';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { AiFillLock } from "react-icons/ai";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
+        Healthy Pets
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -33,7 +38,7 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -59,8 +64,8 @@ export default function Login(props) {
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
@@ -71,18 +76,23 @@ export default function Login(props) {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <AiFillLock />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -92,7 +102,6 @@ export default function Login(props) {
               name="email"
               autoComplete="email"
               autoFocus
-
               className="form-input"
               placeholder="Your email"
               type="email"
@@ -108,7 +117,6 @@ export default function Login(props) {
               type="password"
               id="password"
               autoComplete="current-password"
-
               className="form-input"
               placeholder="******"
               value={formState.password}
